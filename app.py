dd68a4efb3df7dcc16beada7d815ff47c3dd7f5f
MetaDoor Admin <metadoor@example.com>
2026-05-12 15:14:29 +0000

    Enable debug=True to fix Flask routing issue

--- app.py
+++ app.py
@@ -237,4 +237,4 @@ def health():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False, threaded=True)
